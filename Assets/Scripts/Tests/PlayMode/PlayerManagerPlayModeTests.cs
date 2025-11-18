167000f96caa27ffb88127e72d151dfe462141c1
Theodor <Theodor>
2025-11-18 13:59:45 +0200

    feat: Playmode tests added for PlayerManager and EnemiesManager

---
+++ Assets/Scripts/Tests/PlayMode/PlayerManagerPlayModeTests.cs
@@ -0,0 +1,113 @@
+using NUnit.Framework;
+using SpaceInvaders.Scenes.Game;
+using System;
+using System.Collections;
+using NSubstitute;
+using Zenject;
+using Cysharp.Threading.Tasks;
+using UnityEngine.TestTools;
+using System.Linq;
+
+namespace SpaceInvaders.Tests
+{
+    [TestFixture]
+    public class PlayerManagerPlayModeTests : ZenjectUnitTestFixture
+    {
+        private PlayerManager _playerManager;
+        private ISpawnService _mockSpawnService;
+        private IPlayerSpaceship _mockPlayer;
+
+        private IEnumerator InitializeAndSpawnPlayer()
+        {
+            _playerManager.OnGameInitialized();
+            yield return UniTask.WaitUntil(() => _mockPlayer.ReceivedCalls().Any()).ToCoroutine();
+        }
+
+        [SetUp]
+        public override void Setup()
+        {
+            base.Setup();
+
+            _mockSpawnService = Substitute.For<ISpawnService>();
+            _mockPlayer = Substitute.For<IPlayerSpaceship>();
+
+            _mockSpawnService.SpawnPlayer().Returns(UniTask.FromResult(_mockPlayer));
+
+            Container.Bind<ISpawnService>().FromInstance(_mockSpawnService);
+
+            _playerManager = Container.Instantiate<PlayerManager>();
+        }
+
+        [TearDown]
+        public override void Teardown()
+        {
+            _playerManager.Dispose();
+            base.Teardown();
+        }
+
+        [UnityTest]
+        public IEnumerator OnGameInitialized_SpawnsPlayer()
+        {
+            yield return InitializeAndSpawnPlayer();
+            _mockSpawnService.Received(1).SpawnPlayer();
+        }
+
+        [UnityTest]
+        public IEnumerator OnGameInitialized_SubscribesToPlayerDestroyedEvent()
+        {
+            yield return InitializeAndSpawnPlayer();
+
+            _mockPlayer.Received(1).OnDestroyed += Arg.Any<Action<IPlayerSpaceship>>();
+        }
+
+        [UnityTest]
+        public IEnumerator OnGameStarted_EnablesPlayerControls()
+        {
+            yield return InitializeAndSpawnPlayer();
+            _playerManager.OnGameStarted();
+
+            _mockPlayer.Received(1).EnableControls();
+        }
+
+        [UnityTest]
+        public IEnumerator OnPlayerDestroyed_InvokesOnPlayerDestroyedEvent()
+        {
+            var playerDestroyedInvoked = false;
+            _playerManager.OnPlayerDestroyed += () => playerDestroyedInvoked = true;
+
+            yield return InitializeAndSpawnPlayer();
+
+            _mockPlayer.OnDestroyed += Raise.Event<Action<IPlayerSpaceship>>(_mockPlayer);
+            Assert.IsTrue(playerDestroyedInvoked);
+        }
+
+        [UnityTest]
+        public IEnumerator OnPlayerDestroyed_UnsubscribesFromPlayerEvent()
+        {
+            yield return InitializeAndSpawnPlayer();
+
+            _mockPlayer.OnDestroyed += Raise.Event<Action<IPlayerSpaceship>>(_mockPlayer);
+            _mockPlayer.Received(1).OnDestroyed -= Arg.Any<Action<IPlayerSpaceship>>();
+        }
+
+        [UnityTest]
+        public IEnumerator OnGameEnded_DesawnsPlayer()
+        {
+            yield return InitializeAndSpawnPlayer();
+
+            _playerManager.OnGameEnded();
+
+            _mockPlayer.Received(1).OnDestroyed -= Arg.Any<Action<IPlayerSpaceship>>();
+        }
+
+        [UnityTest]
+        public IEnumerator Dispose_DespawnsPlayer()
+        {
+            yield return InitializeAndSpawnPlayer();
+
+            _playerManager.Dispose();
+
+            _mockPlayer.Received(1).OnDestroyed -= Arg.Any<Action<IPlayerSpaceship>>();
+        }
+    }
+}
