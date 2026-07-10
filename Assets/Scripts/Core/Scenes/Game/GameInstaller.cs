9531a270e72c28f4907148d04d816b45d73923a1
Theodor <Theodor>
2026-07-10 12:34:30 +0300

    Repository moved to ProjectInstaller up from GameInstaller

--- Assets/Scripts/Core/Scenes/Game/GameInstaller.cs
+++ Assets/Scripts/Core/Scenes/Game/GameInstaller.cs
@@ -11,9 +11,6 @@ namespace SpaceInvaders.Scenes.Game
         [SerializeField] private Transform _hudContainer;
         [SerializeField] private Transform _gameContainer;
         [SerializeField] private Transform _objectPoolingContainer;
-        
-        [Header("Configs")]
-        [SerializeField] private ConfigsContainerSO _configsContainerSO;
 
         public override void InstallBindings()
         {
@@ -44,9 +41,6 @@ namespace SpaceInvaders.Scenes.Game
         {
             Container.BindInterfacesTo<MessageBus>().AsSingle();
             Container.BindInterfacesTo<ObjectPooling>().AsSingle().WithArguments(_objectPoolingContainer);
-            Container.BindInterfacesTo<RepositoryManager>().AsSingle().WithArguments(
-                _configsContainerSO.LevelsConfigsSO, _configsContainerSO.PlayerConfigsSO, _configsContainerSO.EnemyConfigsSO);
-                
             Container.BindInterfacesTo<CameraManager>().AsSingle();
             Container.BindInterfacesTo<LevelManager>().AsSingle();
             Container.BindInterfacesTo<PlayerManager>().AsSingle();
