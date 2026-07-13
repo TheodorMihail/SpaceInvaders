cc703b0d430e13239feb841e4f40c0099ce33035
Theodor <Theodor>
2026-07-13 23:46:15 +0300

    Powerups logic added to GameplayHUD for visualization

--- Assets/Scripts/Core/Scenes/Game/UI/GameplayHUD/GameplayHUDView.cs
+++ Assets/Scripts/Core/Scenes/Game/UI/GameplayHUD/GameplayHUDView.cs
@@ -1,23 +1,29 @@
+using System.Collections.Generic;
 using System.Threading;
 using BaseArchitecture.Core;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using Zenject;
 
 namespace SpaceInvaders.Scenes.Game
 {
     [AddressablePath("HUD/GameplayHUDView")]
     public class GameplayHUDView : View
     {
+        [Inject] private readonly IObjectPooling _objectPooling;
+
         [SerializeField] private TextMeshProUGUI _scoreText;
         [SerializeField] private TextMeshProUGUI _levelText;
 
         [SerializeField] private string _scoreString = "Score: {0}";
         [SerializeField] private string _levelString = "Level: {0}";
 
-        [SerializeField] private HealthBarComponent _bossHealthBar;
+        [SerializeField] private HealthBarUIComponent _bossHealthBar;
+        [SerializeField] private PowerupIndicatorComponent _powerupIndicatorPrefab;
+        [SerializeField] private Transform _powerupIndicatorsContainer;
 
         private CancellationTokenSource _scoreCancellationTokenSource;
+        private readonly Dictionary<PowerupTypes, PowerupIndicatorComponent> _activePowerupIndicators = new();
         private int _currentScore = 0;
 
         public void Setup(int levelNumber)
@@ -56,6 +62,28 @@ namespace SpaceInvaders.Scenes.Game
         private void FormatScore(int score)
         {
             _scoreText.text = string.Format(_scoreString, score);
+        }
+
+        public void ShowPowerupActivated(PowerupTypes type, Sprite icon, float duration)
+        {
+            if (!_activePowerupIndicators.TryGetValue(type, out var indicator))
+            {
+                indicator = _objectPooling.Get(_powerupIndicatorPrefab, _powerupIndicatorsContainer);
+                _activePowerupIndicators[type] = indicator;
+            }
+
+            indicator.Initialize(icon, duration);
+        }
+
+        public void HidePowerupIndicator(PowerupTypes type)
+        {
+            if (!_activePowerupIndicators.TryGetValue(type, out var indicator))
+            {
+                return;
+            }
+
+            _activePowerupIndicators.Remove(type);
+            _objectPooling.Return(indicator);
         }
 
         private void OnDestroy()
