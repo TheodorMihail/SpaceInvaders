7f1ed842aa8187fbc0d7df4bf8096943331a38e5
Theodor <Theodor>
2026-08-12 17:35:58 +0300

    Levels have dedicated icons based on their type

--- Assets/Scripts/Core/Scenes/MainMenu/UI/LevelSelectionScreen/LevelSelectionView.cs
+++ Assets/Scripts/Core/Scenes/MainMenu/UI/LevelSelectionScreen/LevelSelectionView.cs
@@ -16,6 +16,7 @@ namespace SpaceInvaders.Scenes.MainMenu
         [SerializeField] private LevelButtonComponent _levelButtonPrefab;
         [SerializeField] private Transform _levelButtonsContainer;
         [SerializeField] private Button _backButton;
+        [SerializeField] private List<LevelTypeIconDTO> _levelTypeIcons;
 
         public event Action<int> OnLevelSelectedClicked;
         public event Action OnBackClicked;
@@ -30,9 +31,33 @@ namespace SpaceInvaders.Scenes.MainMenu
             foreach(var level in levels)
             {
                 var button = _factory.CreateFromPrefab(_levelButtonPrefab, _levelButtonsContainer);
-                button.Setup(level, !_model.IsLevelUnlocked(level.Index), _model.GetLevelStars(level.Index));
+                button.Setup(level, GetLevelTypeIcon(level.LevelType), !_model.IsLevelUnlocked(level.Index), _model.GetLevelStars(level.Index));
                 button.OnLevelButtonClicked += OnLevelSelectedClicked;
             }
+        }
+
+        private Sprite GetLevelTypeIcon(LevelTypes levelType)
+        {
+            foreach (LevelTypeIconDTO entry in _levelTypeIcons)
+            {
+                if (entry.LevelType == levelType)
+                {
+                    return entry.Icon;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>Icon shown on a level button, picked by the level's type.</summary>
+        [Serializable]
+        public struct LevelTypeIconDTO
+        {
+            [SerializeField] private LevelTypes _levelType;
+            [SerializeField] private Sprite _icon;
+
+            public LevelTypes LevelType => _levelType;
+            public Sprite Icon => _icon;
         }
     }
 }
